2d9f5916ee823b2341bdfce6d6d74dc274835936
hentzorz <hentzorz14@gmail.com>
2016-11-14 14:13:55 +0100

    FizzBuzz megoldás

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,9 +1,20 @@
 def fizzbuzz(number):
-    return
+    if ((number % 3) == 0) and ((number % 5) == 0):
+       return "FizzBuzz"
+       
+    elif number % 5 == 0:
+        return "Buzz"
+
+    elif number % 3 == 0:
+        return "Fizz"
+
+    return number 
 
 
 def main():
-    return
+    for i in range(0,101):
+        print(fizzbuzz(i)) 
 
 if __name__ == '__main__':
     main()
+
